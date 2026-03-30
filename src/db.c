472df8c543f54204ad4e03e7152b2e38719971ec
Carmen Echeverría Herrera <carmen.echeverria@opendeusto.es>
2026-03-30 09:29:10 +0200

    crear tablas alojamientos, paquetes y transportes en db

--- src/db.c
+++ src/db.c
@@ -38,6 +38,39 @@ int db_crear_tablas(sqlite3 *db) {
         "activo INTEGER NOT NULL DEFAULT 1"
         ");";
 
+    const char *sql_alojamientos =
+            "CREATE TABLE IF NOT EXISTS alojamientos ("
+            "codigo TEXT PRIMARY KEY, "
+            "nombre TEXT NOT NULL, "
+            "direccion TEXT, "
+            "tipo TEXT, "
+            "cod_ciudad TEXT, "
+            "activo INTEGER DEFAULT 1";
+    		");";
+
+    const char *sql_paquetes =
+            "CREATE TABLE IF NOT EXISTS paquetes ("
+            "codigo INTEGER PRIMARY KEY, "
+            "nombre TEXT NOT NULL, "
+            "precio REAL, "
+            "destino TEXT, "
+            "origen TEXT, "
+            "plazas_totales INTEGER, "
+            "plazas_disponibles INTEGER, "
+            "activo INTEGER DEFAULT 1;";
+    		");";
+
+    const char *sql_transportes =
+            "CREATE TABLE IF NOT EXISTS transportes ("
+            "codigo TEXT PRIMARY KEY, "
+            "tipo TEXT, "
+            "fecha_salida TEXT, "
+            "fecha_llegada TEXT, "
+            "id_paquete INTEGER, "
+            "activo INTEGER DEFAULT 1, "
+            "FOREIGN KEY(id_paquete) REFERENCES paquetes(codigo)";
+            ");";
+
     const char *sql_insert_admin =
         "INSERT OR IGNORE INTO administradores (id, usuario, clave) "
         "VALUES (1, 'admin', '1234');";
